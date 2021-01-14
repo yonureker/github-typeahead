1d87f59a8ec809ecfe6870095f8b93188a36e520
yonureker <yonureker@gmail.com>
2021-01-13 18:47:59 -0800

    complete app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useSWR from "swr";
+import React, { useState } from "react";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
+  const [query, setQuery] = useState("");
+  const { data, error } = useSWR(
+    query.length >= 3 ? `https://api.github.com/search/users?q=${query}` : null,
+    fetcher
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div id="search-bar">
+          <input
+            type="search"
+            placeholder="Enter username"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+        <div id="results-container" ref={(node) => node}>
+          {data &&
+            data.hasOwnProperty("items") &&
+            data.items.map((item, index) => (
+              <div id="results-item" key={index}>
+                <div id="profile-photo">
+                  <img src={`${item.avatar_url}`} alt="profile" />
+                </div>
+                <div id="username">
+                  <a href={`${item.html_url}`}>{item.login}</a>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
